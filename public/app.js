d744356a445d3dd7ba1204be64be87d83465edf2
Mike W <mwedler@gmail.com>
2020-07-08 13:19:21 -0500

    working project

--- public/app.js
+++ public/app.js
@@ -1,11 +1,18 @@
-var btn = document.getElementById("submit");
-var p = document.getElementById('p1');
+const btn = document.getElementById("submit");
+const p = document.getElementById('p1');
+const errorP = document.getElementById('error');
+errorP.display = "none";
 p.style.display = "none";
-var selectedCountry = {name: "", code: ""};
+let selectedCountry = {name: "", code: ""};
+let T = 'C';
 
 //creates map pulling data from openstreet and github
 async function createMap(){
-const mymap = L.map('map').setView([0, 0], 1);
+const geojson = await fetch(
+'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson');
+	
+	
+const mymap = L.map('map').setView([20, 0], 2);
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl =
@@ -13,10 +20,10 @@ const tileUrl =
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(mymap);
 
-const geojson = await fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson');
 const geoLayer = await geojson.json();
 L.geoJSON(geoLayer, {
-	onEachFeature: onEachFeature
+	onEachFeature: onEachFeature,
+	style: countryStyle
 }).addTo(mymap);
 }
 
@@ -26,7 +33,24 @@ function onEachFeature(feature, layer) {
 		layer.on('click', function(e){
 			selectedCountry.name = feature.properties.ADMIN
 			selectedCountry.code = feature.properties.ISO_A3;
-		});       		
+		});
+        	
+}
+
+//visual properties for each country
+function countryStyle(feature){
+	return {color: getRandomColor(),
+	        weight: 1}
+}
+
+//color the countries on the map
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 createMap();
@@ -39,13 +63,55 @@ btn.addEventListener('click', async function(event) {
 	let start1 = document.getElementById("start1").value;
 	let end1 = document.getElementById("end1").value;
 	let start2 = document.getElementById("start2").value;
-	let end2 = document.getElementById("end2").value
+	let end2 = document.getElementById("end2").value;
+    let selected = document.getElementById("select");
+    T = selected.options[selected.selectedIndex].value;
+
 	const country = selectedCountry.code;
-	const api_url = `/temperature/${country}/${start1},${end1}/${start2},${end2}`;
+	
+//checks on user input	
+	if(start1 < 1901 || start1 > 2012 || end1 < 1901 || end1 > 2012 ||
+	  start2 < 1901 || start2 > 2012 || end2 < 1902 || end2 > 2012){
+		p.style.display = "none";
+		errorP.style.display = "block";
+		errorP.textContent = "Sorry, no data for that time span!";
+		return;
+	}
+	if(start1 == end1 || start2 == end2){
+		p.style.display = "none";
+		errorP.style.display = "block";
+		errorP.textContent = "Let's try a larger time period";
+		return;
+	}		
+	if(country === ""){
+		p.style.display = "none";
+		errorP.style.display = "block";
+        errorP.textContent = "Woah slow down, you have to select a country";
+		return;
+	}	  
+	if(start1 > end1){
+		const temp1 = start1;
+		start1 = end1;
+		end1 = temp1;
+	}
+	if(start2 > end2){
+		const temp2 = start2;
+		start2 = end2;
+		end2 = temp2;
+	}
+	
+//call for data from server	
+	const api_url = `/temperature/${country}/${start1},${end1}/${start2},${end2}?units=${T}`;
 	
 	const resp = await fetch(api_url);
 	const resp_json = await resp.json();
 	
+	if(resp_json.average1 === null){
+		p.style.display = "none";
+		errorP.style.display = "block";
+		errorP.textContent = `Bummer, no data for ${selectedCountry.name}`;
+		return;
+	}
 	
 	displayResults(resp_json, start1, end1, start2, end2);		  	
 });
@@ -53,6 +119,7 @@ btn.addEventListener('click', async function(event) {
 
 //displays average temperature results
 function displayResults(data, start1, end1, start2, end2){
+	errorP.style.display = "none";
 	p.style.display = "block";
 	document.getElementById('country').textContent = selectedCountry.name;
 	document.getElementById('s1').textContent = start1; 
@@ -61,6 +128,8 @@ function displayResults(data, start1, end1, start2, end2){
 	document.getElementById('e2').textContent = end2;
 	document.getElementById('temp1').textContent = data.average1;
 	document.getElementById('temp2').textContent = data.average2;
+	document.getElementById('u1').textContent = T;
+	document.getElementById('u2').textContent = T;
 	
 	
 }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyparser = require("body-parser");
 const fetch = require('node-fetch');
 
 const app = express();
-const port = process.env.PORT || 8200;
+const port = process.env.PORT || 8400;
 app.listen(port, () => {
   console.log(`Starting server at ${port}`);
 });
@@ -16,38 +16,33 @@ app.use(express.static('public'));
 //server requesting information from worldbank
 app.get('/temperature/:country/:years1/:years2', async (request, response) => { 
 	try{
-	const temp_url = `http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/${request.params.country}`;
+	const units = request.query.units;
+	const temp_url = 
+	`http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/${request.params.country}`;
 	 
-    const temp_response = await fetch(temp_url);
-	
+    const temp_response = await fetch(temp_url);	
 	const temp_data = await temp_response.json();
-	
-	
 	
 	const years1 = request.params.years1.split(',');
 	const years2 = request.params.years2.split(',');
 	
-    let avg1 = averageTemp(temp_data, years1);
-	let avg2 = averageTemp(temp_data, years2);
+    let avg1 = averageTemp(temp_data, years1, units);
+	let avg2 = averageTemp(temp_data, years2, units);
 	
 	const data = {
 	  average1 : avg1,
 	  average2 : avg2
 	};
-	//console.log(data);
+
 	response.send(JSON.stringify(data));
 	
 	} catch (error){
 		console.log(error)
 	}
 });
-app.get('/test', (req, resp) => {
-	console.log('test');
-	resp.send("ok");
-});
 
 //function for calculating average temperature of data given years
-function averageTemp(data, years){
+function averageTemp(data, years, units){
 	const yearStart = years[0];
 	const yearEnd = years[1];
 	
@@ -60,9 +55,12 @@ function averageTemp(data, years){
 		tempTotal = tempTotal + obj.data;
 		count++;
 	}
+	if(units === "C"){
 	return roundTo(tempTotal/count, 2);
+	}
+	return roundTo(convertF(tempTotal/count),2);
 	
-	
+    		
 }
 
 //function for rounding temperature averages
@@ -77,7 +75,9 @@ function roundTo(n, digits) {
      return +(test.toFixed(digits));
 }
 
-
+function convertF(n){
+	return n*(9/5) + 32;
+}
 
 
 
